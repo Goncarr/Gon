eed74b8edec881a36dc00ec9b5996bf737304245
Gon <goncalo.arruda632@gmail.com>
2026-07-08 06:03:36 +0000

    site should be finishe by now

--- src/components/aboutMe/About.js
+++ src/components/aboutMe/About.js
@@ -3,7 +3,7 @@ export const aboutMe = [
         key: "1",
         sona: "gon",
         image: "art/gon-art1.webp",
-        text: "Hi i'm Gon! I'm a 22 year old guy from Portugal\n and i enjoy meeting new peeps! Feel free\n to add me",
+        text: "Hi i'm Gon! I'm a 22 year old guy from Portugal\n and i enjoy meeting new people! Feel free to\nadd me on my socials",
         alt: "That's me bro"
     },
 
@@ -11,7 +11,7 @@ export const aboutMe = [
         key: "2",
         sona: "banshee",
         image: "sonas/gravedigger.png",
-        text: 'Name: Banshee\nAge:25\nheight: 5"8\nStory:He"s cool pemni and awesome and yeah',
+        text: 'Name: Banshee\nAge:25\nheight: 5"8',
         alt: "Basnhee"
     }
 ]
